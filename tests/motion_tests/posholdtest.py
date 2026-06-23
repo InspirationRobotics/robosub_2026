aac4a27ea703b66606ad5d02945837c935938697
carinad90 <152536012+carinad90@users.noreply.github.com>
2026-06-22 20:19:22 -0700

    Update posholdtest.py

--- tests/motion_tests/posholdtest.py
+++ tests/motion_tests/posholdtest.py
@@ -4,27 +4,22 @@ from auv.motion import robot_control
 from auv.utils import arm, disarm
 if __name__ == "__main__":
     try:
-        rospy.init_node("poshold_test", anonymous=True)  # avoid hiearchy issue
+        rospy.init_node("station_keeping_test", anonymous=True)  # avoid hiearchy issue
         rc = robot_control.RobotControl()
         rc.set_control_mode("pid")
         arm.arm()
         time.sleep(2.0)
-        print("[INFO}This is the start")
+        print("[INFO}This is the start of station keeping test")
         rc.set_absolute_yaw(0)
         rc.set_absolute_x(0)
         rc.set_absolute_y(0)
         rc.set_absolute_z(0.3)
-        rospy.loginfo("starting...")
-        time.sleep(30)
+        rospy.loginfo("start station keeping for 60s...")
+        time.sleep(60)
 
-        rc.reset()
-        rospy.loginfo("Setting yaw to 90")
-        rc.set_absolute_yaw(90)
-        time.sleep(10)
         rospy.loginfo("Reached the end of the program")
         rc.reset()
         rc.exit()
         disarm.disarm()
     except KeyboardInterrupt:
         disarm.disarm()
-        
