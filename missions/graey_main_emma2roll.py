e34e83baf2bafac4100eb1946f8dd9a2f55ce300
EMC641 <108538423+EMC641@users.noreply.github.com>
2026-06-26 21:28:04 -0700

    roll manuver geraey test

---
+++ missions/graey_main_emma2roll.py
@@ -0,0 +1,37 @@
+"""
+To create a sequential order of missions for Graey to follow.
+"""
+
+import rospy
+import time
+from auv.utils import deviceHelper
+from auv.mission import intersub_com_mission, gate_intersub_mission
+from auv.motion import robot_control
+from auv.utils import arm, disarm, deviceHelper
+
+"""INITIALIZE"""
+rospy.init_node("Graey", anonymous = True)
+rc = robot_control.RobotControl()
+rc.set_control_mode('depth_hold')
+rc.set_flight_mode("STABILIZE")
+rc.go_to_depth(0.5)
+rospy.loginfo("Robot armed and set to depth 0.8 m")
+gate_heading = 0 # CALIBRATE EACH TIME 
+return_heading = 180
+config = deviceHelper.variables
+eventflags = [False,False,False,False,False]
+
+
+
+"""ROLL MANEUVER"""
+try:
+    intersubMission.do_roll()  # <-- Execute roll here
+except Exception as e:
+    rospy.logerr("ERROR OCCUR DURING ROLL MANEUVER")
+    rospy.logerr(e)
+    
+
+disarm.disarm()
+rc.exit()
+
+
