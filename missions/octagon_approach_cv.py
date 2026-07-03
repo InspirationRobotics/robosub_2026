72ec1728ad608d3f02f18e5514414832b1430035
1npk412 <1npk412@gmail.com>
2026-07-03 12:58:45 -0700

    Update octagon_approach_cv.py

--- missions/octagon_approach_cv.py
+++ missions/octagon_approach_cv.py
@@ -16,7 +16,7 @@ class CV:
 
     # Camera to get the camera stream from.
     camera = "/auv/camera/videoOAKdRawForward"
-    model = "poles" # Change later once data is collected for the platform
+    model = "octagonTest" # Change later once data is collected for the platform
 
     def __init__(self, **config):
         """
