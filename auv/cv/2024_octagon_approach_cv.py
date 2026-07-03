767e17a5289205d05e317b266d85afd65738a8ba
1npk412 <1npk412@gmail.com>
2026-07-02 18:27:16 -0700

    Update 2024_octagon_approach_cv.py

--- auv/cv/2024_octagon_approach_cv.py
+++ auv/cv/2024_octagon_approach_cv.py
@@ -1,20 +1,21 @@
 """
-Bn Approach CV. Finds the bin, and approaches the bin until it is either big enough or can no longer see it.
+Octagon Approach CV. Finds the octagon, and approaches the octagon until it can no longer see it.
 """
 
 import time
 
 import cv2
 import numpy as np
+import time
 
 class CV:
     """
-    Bin Approach CV class. DO NOT change the name of the class, as this will mess up all of the backend files to run the CV scripts.
+    Octagon Approach CV class. DO NOT change the name of the class, as this will mess up all of the backend files to run the CV scripts.
     """
 
     # Camera to get the camera stream from.
     camera = "/auv/camera/videoOAKdRawForward"
-    model = "bin_approach" 
+    model = "octagonTest" # Change later once data is collected for the platform
 
     def __init__(self, **config):
         """
@@ -39,21 +40,21 @@ class CV:
         self.last_yaw = 0
         self.yaw_time_search = 2
         self.end = False
+        self.prev_time = time.time()
         
-
-        print("[INFO] Bin Approach CV Initialization")
+        print("[INFO] Octagon Approach CV Initialization")
 
     def smart_approach(self, detection_x):
         """Function to properly yaw and move forward"""
         forward = 0
         # Yaw cannot go below 0.5
         if detection_x < self.x_midpoint - self.tolerance:
-            yaw = -0.6
+            yaw = -0.75
         elif detection_x > self.x_midpoint + self.tolerance:
-            yaw = 0.6
+            yaw = 0.75
         else:
             yaw = 0
-            forward = 1
+            forward = 2.0
 
         return forward, yaw
 
@@ -85,16 +86,23 @@ class CV:
         # Align with the bin and move forward (through strafe should be fine)
         # If we have lost sight of the bin, then end
 
+        # So we do not get a NoneType error
+        if detections is None:
+            detections = []
         if len(detections) == 0 and self.prev_detected == False:
             self.state = "search"
         
         if len(detections) == 0 and self.prev_detected == True:
-            self.end = True
+            if time.time() - self.prev_time < 2:
+                self.state = None
+                forward = 0
+            else:
+                self.end = True
 
         if len(detections) >= 1:
             if len(detections) == 1:
                 for detection in detections:
-                    # print(f"[DEBUG] Detection confidence: {detection.confidence}")
+                    print(f"[DEBUG] Detection confidence: {detection.confidence}")
                     if detection.confidence > 0.65:
                         target_x = (detection.xmin + detection.xmax) / 2
                         target_y = (detection.ymin + detection.ymax) / 2
@@ -105,7 +113,10 @@ class CV:
             elif len(detections) > 1:
                 # Target the detection with the highest confidence. The detection targeted
                 # doesn't matter since this is a localization script, not a mission script
-                detection_confidence = 0
+
+                # Increased required confidence to 0.65 to account for multiple false positives without
+                # true positive
+                detection_confidence = 0.65
                 for detection in detections:
                     if detection.confidence > detection_confidence and detection.confidence > 0.65:
                         target_x = (detection.xmin + detection.xmax) / 2
@@ -119,25 +130,14 @@ class CV:
             self.state = "approach"
 
         if self.state == "search":
-            if self.start_time == None:
-                self.start_time = time.time()
-                self.last_yaw = 1.0  # Initial direction
-
-            elapsed_time = time.time() - self.start_time
-
-            if elapsed_time < self.yaw_time_search:
-                yaw = self.last_yaw
-            else:
-                # Switch direction and reset timer
-                self.last_yaw = -self.last_yaw
-                self.start_time = time.time()
-                yaw = self.last_yaw
-                self.yaw_time_search += 1.5
+            # Scrap search grid in favor of circular search
+            yaw = 1
 
         if self.state == "approach":
-            # print("[DEBUG] Approaching now!")
-            # print(target_x)
+            print("[DEBUG] Approaching now!")
+            print(target_x)
             forward, yaw = self.smart_approach(target_x)
+            self.prev_time = time.time()
             
 
         # Continuously return motion commands, the state of the mission, and the visualized frame.
