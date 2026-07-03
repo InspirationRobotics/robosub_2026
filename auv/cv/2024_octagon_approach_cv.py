cef371424d9e0350de94165513c97a9cd7b05869
1npk412 <1npk412@gmail.com>
2026-07-03 15:23:17 -0700

    Update 2024_octagon_approach_cv.py

--- auv/cv/2024_octagon_approach_cv.py
+++ auv/cv/2024_octagon_approach_cv.py
@@ -15,7 +15,7 @@ class CV:
 
     # Camera to get the camera stream from.
     camera = "/auv/camera/videoOAKdRawForward"
-    model = "octagonTest" # Change later once data is collected for the platform
+    model = "octagon_woolett" # Change later once data is collected for the platform
 
     def __init__(self, **config):
         """
