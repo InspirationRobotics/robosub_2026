4644ec5365ee71598a13f9051c8cc9fb2838593b
1npk412 <1npk412@gmail.com>
2026-07-10 13:39:49 -0700

    Update depth_test.py

--- tests/motion_tests/depth_test.py
+++ tests/motion_tests/depth_test.py
@@ -14,9 +14,8 @@ print("[INFO] Setting depth to 0.4 meters")
 
 time.sleep(20.0)
 
-rc.set_relative_depth(-0.2)
-print("[INFO] Changing depth by +0.2 meters")
+rc.go_to_depth(0.0)
+print("[INFO] Changing depth to 0")
 
-time.sleep(20.0)
 
 disarm.disarm()
