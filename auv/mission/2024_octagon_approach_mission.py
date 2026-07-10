1d93a1eadc4c647cf20906e4e6124e14bcf42641
1npk412 <1npk412@gmail.com>
2026-07-10 14:16:14 -0700

    Update 2024_octagon_approach_mission.py

--- auv/mission/2024_octagon_approach_mission.py
+++ auv/mission/2024_octagon_approach_mission.py
@@ -97,8 +97,9 @@ class OctagonApproachMission:
             else:
                 if forward > abs(yaw):
                     self.robot_control.set_flight_mode('depth_hold')
-                    self.robot_control.go_forward_distance(0.3)
-                    print('WE IS GOING FORWARDDDDDDDDDDDDDDD')
+                    time.sleep(0.1)
+                    self.robot_control.go_forward_distance(1)
+                    print('WE IS GOING FORWARDDDDDDDDDDDDDDDWE IS GOING FORWARDDDDDDDDDDDDDDDWE IS GOING FORWARDDDDDDDDDDDDDDD')
                 else:
                     self.robot_control.set_flight_mode("STABILIZE")
                     self.robot_control.movement(lateral = lateral, forward = forward, yaw = yaw, vertical = vertical)
