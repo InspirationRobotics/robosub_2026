57e1d461688d463c21522aa8ba2202f8cc25468c
RT-2010 <Ruthtu2021@gmail.com>
2026-07-12 14:00:28 -0700

    Refactor onyx_2026_main.py by removing missions
    Removed multiple mission sections and updated waypoint file.

--- missions/onyx_2026_main.py
+++ missions/onyx_2026_main.py
@@ -6,14 +6,11 @@ import rospy
 import time
 import json
 
-print("10s before onyx initialize")
-time.sleep(10)
-
-from auv.mission import poles_mission_right, bins_approach2026_mission, octagon_approach_mission, intersub_com_mission, torpedo_approach_mission
+from auv.mission import poles_mission_right
 from auv.motion import robot_control
 from auv.utils import arm, disarm, deviceHelper
 
-with open("./missions/waypoints_delta.json", "r") as file:
+with open("./missions/A7-12.json", "r==") as file:
     data = json.load(file)
 
 # Build a dictionary where the keys are the labels (e.g., "G1", "S1")
@@ -49,20 +46,10 @@ rc.go_to_depth(0.6)
 try:
     #COIN FLIP
     rc.go_to_heading(0)
-    navigate_to("G1")
+    navigate_to("S1")
     rospy.loginfo("GATE MISSION FINISHED")
 except Exception as e:
     rospy.logerr("ERROR DOING GATE MISSION")
-    rospy.logerr(e)
-
-"""INTERSUB MISSION"""
-try:
-    rospy.loginfo("Intersub Communication")
-    Intersub = intersub_com_mission.IntersubMission(robotControl=rc)
-    Intersub.run()
-    rospy.loginfo("INTERSUB MISSION FINISHED")
-except Exception as e:
-    rospy.logerr("ERROR DOING INTERSUB MISSION")
     rospy.logerr(e)
 
 """SLALOM MISSION"""
@@ -78,83 +65,6 @@ except Exception as e:
     rospy.logerr("ERROR OCCUR IN POLES MISSION")
     rospy.logerr(e)
 
-"""INTERSUB MISSION"""
-try:
-    rospy.loginfo("Intersub Communication")
-    Intersub = intersub_com_mission.IntersubMission(robotControl=rc)
-    Intersub.run()
-    rospy.loginfo("INTERSUB MISSION FINISHED")
-except Exception as e:
-    rospy.logerr("ERROR DOING INTERSUB MISSION")
-    rospy.logerr(e)
-
-"""BIN MISSION"""
-try:
-    navigate_to("B1")
-    binApproachMission = bins_approach2026_mission.BinsApproachMission(robotControl=rc)
-    binApproachMission.run()
-    binApproachMission.cleanup()
-    rospy.loginfo("BIN APPROACH MISSION FINISHED")
-
-    binDropMission = bin_drop_mission.BinsDropMission(robotControl=rc)
-    binDropMission.run()
-    binDropMission.cleanup()
-
-    rospy.loginfo("BIN DROP MISSION FINISHED")
-except Exception as e:
-    rospy.logerr("ERROR DOING BIN MISSION")
-    rospy.logerr(e)
-
-"""INTERSUB MISSION"""
-try:
-    rospy.loginfo("Intersub Communication")
-    Intersub = intersub_com_mission.IntersubMission(robotControl=rc)
-    Intersub.run()
-    rospy.loginfo("INTERSUB MISSION FINISHED")
-except Exception as e:
-    rospy.logerr("ERROR DOING INTERSUB MISSION")
-    rospy.logerr(e)
-
-"""OCTAGON MISSION"""
-try:
-    navigate_to("O1")
-    octagonMission = octagon_approach_mission.OctagonMission(robotControl=rc)
-    octagonMission.run()
-    rospy.loginfo("OCTAGON MISSION FINISHED")
-except Exception as e:
-   rospy.logerr("ERROR DOING OCTAGON MISSION")
-   rospy.logerr(e)
-
-"""INTERSUB MISSION"""
-try:
-    rospy.loginfo("Intersub Communication")
-    Intersub = intersub_com_mission.IntersubMission(robotControl=rc)
-    Intersub.run()
-    rospy.loginfo("INTERSUB MISSION FINISHED")
-except Exception as e:
-    rospy.logerr("ERROR DOING INTERSUB MISSION")
-    rospy.logerr(e)
-
-"""TORPEDO MISSION"""
-try:
-    navigate_to("T1")
-    torpedoMission = torpedo_approach_mission.TorpedoApproachMission(robotControl=rc)
-    torpedoMission.run()
-    rc.move_servo("/auv/devices/torpedo")    
-except Exception as e:
-    rospy.logerr("ERROR DOING TORPEDO MISSION")
-    rospy.logerr(e)
-
-"""INTERSUB MISSION"""
-try:
-    rospy.loginfo("Intersub Communication")
-    Intersub = intersub_com_mission.IntersubMission(robotControl=rc)
-    Intersub.run()
-    rospy.loginfo("INTERSUB MISSION FINISHED")
-except Exception as e:
-    rospy.logerr("ERROR DOING INTERSUB MISSION")
-    rospy.logerr(e)
-
 """RETURN HOME MISSION"""
 try:
     rospy.loginfo("Return Home")
@@ -167,3 +77,15 @@ except Exception as e:
 print("[INFO] Mission run terminate")
 disarm.disarm()
 rc.exit()
+
+#######
+"""TEMPLATE MISSION"""
+"""try:
+    rospy.loginfo("Test Mission")
+    Mission = test.test(robotControl=rc)
+    Mission.run()
+    rospy.loginfo("MISSION FINISHED")
+except Exception as e:
+    rospy.logerr("ERROR DOING MISSION")
+    rospy.logerr(e)
+"""
