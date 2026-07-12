a24ec4344586514dc77ed72b75b6f4d22e06a2af
RT-2010 <Ruthtu2021@gmail.com>
2026-07-12 12:03:01 -0700

    Add Bins Approach Mission implementation
    Implement the Bins Approach Mission for the AUV, including initialization, CV handling, and movement control based on CV data.

---
+++ auv/mission/bins_approach2026_mission.py
@@ -0,0 +1,152 @@
+"""
+Octagon Approach Mission. The goal is to find the Octagon, and then switch to bottom facing camera to align with the center 
+of the platform before surfacing.
+
+NOTE: We could just use DVL to get inside the Octagon range if that works consistently enough.
+"""
+import importlib
+import json
+import rospy
+import time
+from std_msgs.msg import String
+from auv.device import cv_handler # For running mission-specific CV scripts
+from auv.motion import robot_control # For running the motors on the sub
+from auv.utils import arm, disarm
+from auv.mission import bins_drop2026_mission
+
+class BinsApproachMission:
+    cv_files = ["bins_approach2026_cv"] # CV file to run
+
+    def __init__(self, target=None, **config):
+        """
+        Initialize the mission class; here should be all of the things needed in the run function. 
+
+        Args:
+            config: Mission-specific parameters to run the mission.
+        """
+        self.config = config
+        self.data = {}  # Dictionary to store the data from the CV handler
+        self.next_data = {}  # Dictionary to store the newest data from the CV handler; this data will be merged with self.data.
+        self.received = False
+
+        self.robot_control = robot_control.RobotControl()
+        self.robot_control.set_flight_mode("STABILIZE") #reduces yaw when desending somehow
+        self.robot_control.go_to_depth(0.45)
+        self.cv_handler = cv_handler.CVHandler(**self.config)
+        self.end_heading = self.robot_control.get_heading()
+        # Initialize the CV handlers; dummys are used to input a video file instead of the camera stream as data for the CV script to run on
+        for file_name in self.cv_files:
+            self.cv_handler.start_cv(file_name, self.callback)
+
+        self.cv_handler.set_target("bins_approach2026_cv", target)
+        print("[INFO] BinsApproach Init")
+    def callback(self, msg):
+        """
+        Calls back the cv_handler output -- you can have multiple callbacks for multiple CV handlers. Converts the output into JSON format.
+
+        Args:
+            msg: cv_handler output -- this will be a dictionary of motion commands and potentially the visualized frame as well as servo commands (like the torpedo launcher)
+        """
+        file_name = msg._connection_header["topic"].split("/")[-1] # Get the file name from the topic name
+        data = json.loads(msg.data) # Convert the data to JSON
+        self.next_data[file_name] = data 
+        self.received = True
+
+        # print(f"[DEBUG] Received data from {file_name}")
+
+    def run(self):
+        """
+        Here should be all the code required to run the mission.
+        This could be a loop, a finite state machine, etc.
+        """
+        while not rospy.is_shutdown():
+            time.sleep(0.05)
+            if not self.received:
+                continue
+
+            # Merge self.next_data, which contains the updated CV handler output, with self.data, which contains the previous CV handler output.
+            # self.next_data will be wiped so that it can be updated with the new CV handler output.
+            for key in self.next_data.keys():
+                if key in self.data.keys():
+                    self.data[key].update(self.next_data[key]) # Merge the data
+                else:
+                    self.data[key] = self.next_data[key] # Update the keys if necessary
+            self.received = False
+            self.next_data = {}
+
+            # Do something with the data.
+            lateral = self.data["bins_approach2026_cv"].get("lateral", None)
+            forward = self.data["bins_approach2026_cv"].get("forward", None)
+            yaw = self.data["bins_approach2026_cv"].get("yaw", None)
+            vertical = self.data["bins_approach2026_cv"].get("vertical", None)
+            end = self.data["bins_approach2026_cv"].get("end", None)
+
+            if end:
+                print("Ending Bins Approach")
+                self.robot_control.movement(lateral = 0, forward = 0, yaw = 0)
+                self.robot_control.set_flight_mode("STABILIZE")
+                #self.robot_control.go_forward_distance(1) #1 meter distance offset, change based on how sub does 
+                break
+            else:
+                if forward > abs(yaw): #yes this line is dumb but yes it works bc yaw is always like 0 if forward is set
+                    self.robot_control.set_flight_mode('depth_hold')
+                    time.sleep(0.1)
+                    print('WE IS GOING FORWARDDDDDDDDDDDDDDDWE IS GOING FORWARDDDDDDDDDDDDDDDWE IS GOING FORWARDDDDDDDDDDDDDDD')
+                    self.robot_control.go_forward_distance(1)
+                else:
+                    self.robot_control.set_flight_mode("STABILIZE")
+                    self.robot_control.movement(lateral = lateral, forward = forward, yaw = yaw, vertical = vertical)
+                print('YAW: ', yaw)
+
+        #going to 0.2 depth to prepare for bin dropping
+        print('going to 0.2 for bins drop')
+        self.robot_control.set_flight_mode("STABILIZE")
+        self.robot_control.go_to_depth(0.2)
+        time.sleep(3)
+        print("Bins Approach Complete")
+
+    def cleanup(self):
+        """
+        Here should be all the code required after the run function.
+        This could be cleanup, saving data, closing files, etc.
+        """
+        for file_name in self.cv_files:
+            self.cv_handler.stop_cv(file_name)
+
+        # Idle the robot
+        self.robot_control.movement(lateral = 0, forward = 0, yaw = 0)
+        print("[INFO] Octagon approach mission terminate")
+
+
+if __name__ == "__main__":
+    # This is the code that will be executed if you run this file directly
+    # It is here for testing purposes
+    # you can run this file independently using: "python -m auv.mission.template_mission"
+    # You can also import it in a mission file outside of the package
+    import time
+    from auv.utils import deviceHelper
+    from auv.motion import robot_control
+    rospy.init_node("bins_approach2026_mission", anonymous=True)
+
+    config = deviceHelper.variables
+    config.update(
+        {
+            # # this dummy video file will be used instead of the camera if uncommented
+            # "cv_dummy": ["/somepath/thisisavideo.mp4"],
+        }
+    )
+
+    # Create a mission object with arguments
+    mission = BinsApproachMission(**config)
+    #mission2 = BinsDropMission(**config)
+    # Run the mission
+
+    arm.arm()
+
+    mission.run()
+    mission.cleanup()
+    #Use below to run drop mission if you want
+    #mission2.run()
+    #mission2.cleanup()
+
+    disarm.disarm()
