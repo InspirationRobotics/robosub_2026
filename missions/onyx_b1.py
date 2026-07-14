0db61b2e65a09ee756365a33f0541494fd999877
RT-2010 <Ruthtu2021@gmail.com>
2026-07-14 09:28:45 -0700

    Update Onyx mission sequence with communication features
    Refactor Onyx mission sequence to include coin toss and communication with Graey. Adjust robot control settings and mission flow.

--- missions/onyx_b1.py
+++ missions/onyx_b1.py
@@ -1,92 +1,81 @@
+#this mission sequence is for Onyx to perform Gate, Coin flip, and intersub
 """
-Perform gate, poles, bins, torpedoes, octagon, coms 
+ONYX Mission SEQUENCE
 """
+import rospy # for ROS node and logging
+import time # for time sleep and delays for semi-finals
 
-import rospy
-import time
-import json
-
-rospy.loginfo("waiting for 30 s for tether disconnection")
-time.sleep(30) # wait for tether disconnection
-
-from auv.mission import poles_mission, poles_with_com_mission, bin_approach_mission, bin_drop_mission, octagon_approach_mission, intersub_com_mission, torpedo_approach_mission, gate_intersub_mission
+from auv.mission import communication_helper_revised
 from auv.motion import robot_control
 from auv.utils import arm, disarm, deviceHelper
-
-def navigate_with_heading(name):
-    Waypoint = waypoints[name]
-    rc.waypointNav(Waypoint["position"][0],Waypoint["position"][1])
-    rc.go_to_heading(Waypoint["heading"])
-    rospy.loginfo(f"Reached {name} waypoint")
-
+\
 """INITIALIZE"""
 rospy.init_node("Onyx", anonymous = True)
 rc = robot_control.RobotControl()
+rc.set_control_mode('depth_hold')
 rc.set_flight_mode("STABILIZE")
-rc.set_control_mode("depth_hold")
+rc.go_to_depth(1)
+rospy.loginfo("Robot armed and set to depth 1 m")
+comms = communication_helper_revised.intersubComMission(robotControl=rc)
+gate_heading = 0 # CALIBRATE EACH TIME 
+return_heading = 180
 config = deviceHelper.variables
+GRAEY_ADDR = "010"
 
-# Load the JSON file
-with open("./missions/waypoints_b1.json", "r") as file:
-    waypoints = json.load(file)
-
-# Dive down to desire depth
-rc.go_to_depth(1.2)
-
-rospy.loginfo("Finish initialization")
-
-
-"""GATE MISSION"""
+#throughout the missions want to send repeated messages to Graey
+"""COIN TOSS + GATE MISSION"""
 try:
-    # COIN FLIP
-    rc.go_to_heading(0)
-    rc.activate_heading_control(True)
-    rc.set_absolute_yaw(0)
-    rc.go_forward_distance(5.57)
-    rc.go_lateral_distance(0.8)
-    rospy.loginfo("GATE MISSION FINISHED")
+   rc.go_to_heading(gate_heading)
+   rc.activate_heading_control(True)
+   rc.set_absolute_yaw(gate_heading)
+   rospy.loginfo("Robot heading set to gate heading")
+   
+   rospy.loginfo(f"Start moving forward")
+   comms.send_repeated(dest_addr=GRAEY_ADDR,message="GATE_START",count=5,delay=0.5)
+   rc.movement(forward=2)
+   time.sleep(10)
+   rc.movement()
+   rospy.loginfo("[INFO] GATE MISSION COMPLETE")
+
+   #send message to Graey that gate mission is complete
+   comms.send_repeated(dest_addr=GRAEY_ADDR,message="GATE_FINISH",count=5,delay=0.5)
 except Exception as e:
     rospy.logerr("ERROR DOING GATE MISSION")
     rospy.logerr(e)
 
-"""GATE INTERSUB MISSION"""
+# back through the gate
 try:
-    rospy.loginfo("Gate Intersub ")
-    gateIntersub = gate_intersub_mission.GateIntersubMission(robotControl=rc)
-    gateIntersub.run()
-    rospy.loginfo("GATE INTERSUB MISSION FINISHED")
+    comms.send_repeated(dest_addr=GRAEY_ADDR,message="RETURN_START",count=5,delay=0.5)
+    rc.movement(forward=-2)
+    time.sleep(10)
+    rc.movement()
+    comms.send_repeated(dest_addr=GRAEY_ADDR,message="RETURN_FINISH",count=5,delay=0.5)
+    rospy.loginfo("FINSHED RETURNING HOME")
 except Exception as e:
-    rospy.logerr("ERROR DOING GATE MISSION")
+    rospy.logerr("ERROR OCCUR IN RETURNING HOME")
     rospy.logerr(e)
 
-"""SLALOM MISSION"""
-# navigate_with_heading("S1")
-try: 
-    # Run the poles mission
-    rc.activate_heading_control(False)
-    rc.go_to_heading(28)
+try:
+    comms.send_repeated(dest_addr=GRAEY_ADDR,message="ROLL_START",count=5,delay=0.5)
+    #rc.go_to_depth(0.7)
+    rospy.loginfo("Performing roll")
+    rc.set_flight_mode("ACRO")
+    rc.set_control_mode("direct")
+    rc.movement(roll=5)
+    time.sleep(4)
+
+    rc.movement()
     time.sleep(2)
-    rc.activate_heading_control(True)
-    rc.set_absolute_yaw(28)
-    rospy.loginfo("Start of poles mission...")
-    poles = poles_with_com_mission.PoleSlalomMission(rc=rc,**config)
-    poles.run()
-    poles.cleanup()
-    print("[INFO] POLES MISSION COMPLETE")
+
+    rc.set_flight_mode("STABILIZE")
+    rc.set_control_mode("depth_hold")
+    comms.send_repeated(dest_addr=GRAEY_ADDR,message="ROLL_END",count=5,delay=0.5)
+    rospy.loginfo("FINSHED STYLE")
 except Exception as e:
-    rospy.logerr("ERROR OCCUR IN POLES MISSION")
+    rospy.logerr("ERROR OCCUR DURING STYLE")
     rospy.logerr(e)
 
-
-"""MODEMS + ROLL"""
-try:
-    intersubMission = intersub_com_mission.intersubComMission(robotControl=rc)
-    intersubMission.run()
-    rospy.loginfo("FINISHED INTERSUB COMMUNICATION")
-except Exception as e:
-    rospy.logerr("ERROR DURING MODEM MISSION")
-    rospy.logerr(e)
-
+comms.send_repeated(dest_addr=GRAEY_ADDR,message="ONYX_ENDING_RUN",count=5,delay=0.5)
 print("[INFO] Mission run terminate")
 disarm.disarm()
 rc.exit()
