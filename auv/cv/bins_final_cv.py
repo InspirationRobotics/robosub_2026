af7da03a24ef264fac2c28c1db6b2ed4299fad1d
1npk412 <1npk412@gmail.com>
2026-07-15 13:17:42 -0700

    Made Bins_FINAL!!!!!!!
    FINAL FINALS FINALS FINALS, I WILL ###@&!*#!*!@! IF WE MAKE IT

---
+++ auv/cv/bins_final_cv.py
@@ -0,0 +1,215 @@
+"""
+Bins CV. Center's the sub's camera on the bin 
+"""
+import math
+import time
+import cv2 
+import numpy as np
+import time
+import math
+class CV:
+    """
+    Bins CV class. DO NOT change the name of the class, as this will mess up all of the backend files to run the CV scripts.
+    """
+
+    # Camera to get the camera stream from.
+    camera = "/auv/camera/videoOAKdRawBottom"
+    model = "bins_icon" # Change later once data is collected for the platform
+
+    def __init__(self, **config):
+        """
+        Initialize the CV class. 
+        Setup/attributes here will contain everything needed for the run function.
+        
+        Args:
+            config: Dictionary that contains the configuration of the devices on the sub.
+        """
+
+        self.config = config
+        self.shape = (640, 480)
+        self.x_midpoint = self.shape[0]/2
+        self.y_midpoint = self.shape[1]/2
+        self.missed_frames = 0
+        self.tolerance = 50
+        self.prev_detected = False
+        self.state = None
+        self.start_time = None
+        self.stage = "lateral" #we center laterally first, stage for centering onto bin 
+        self.drop = False
+        self.prev_time = time.time()
+        self.search_time = 0
+        self.wait = False
+        self.phase = None
+        print("[INFO] Bins Drop CV Initialization")
+
+    def center_x(self, target_x):
+        '''takes the detected center of the bin and returns the lateral value to move the sub towards it'''
+        offset = target_x - self.x_midpoint
+        proportion = 0.205 if offset > 0 else -0.205 # the distance decides thruster power
+        lateral = proportion * (abs(offset) ** 0.275) 
+        self.state = "centering x"
+        return lateral
+    
+    def center_y(self, target_y):
+        '''takes the detected center of the bin and returns the forward value to move the sub towards it'''
+        offset = target_y - self.x_midpoint
+        proportion = 0.168 if offset > 0 else -0.168 # the distance decides thruster power
+        forward = proportion * (abs(offset) ** 0.326) 
+        self.state = "centering x"
+        return forward
+
+    ########
+    def search(self, frame, target, detections):
+            self.wait = True
+            #calls centering if phase is set, else we search
+            lateral = 0 #default
+            forward = 0 #default
+            if detections is None:
+                detections = []
+
+            if len(detections) > 0:
+                self.phase = 'centering'
+                return
+            i = i + 1
+            if i == 1:
+                lateral = 0.5
+            if i == 2:
+                forward = 0.5
+            if i == 3:
+                lateral = -0.5
+            if i == 4:
+                forward = 0.5
+                i = 0
+            return lateral, forward
+
+    ########
+
+    def run(self, frame, target, detections):
+        """
+        Run the CV script.
+        Args:
+            frame: The frame from the camera stream
+            target: This can be any type of information, for example, the object to look for
+            detections: This only applies to OAK-D cameras; this is the list of detections from the ML model output
+
+        Here should be all the code required to run the CV.
+        This could be a loop, grabbing frames using ROS, etc.
+
+        Returns:
+            dictionary, visualized frame: {motion commands/flags for servos and other indication flags}, visualized frame
+        """
+        forward = 0
+        lateral = 0
+        yaw = 0
+        vertical = 0
+            
+
+        target_x = None
+        target_y = None
+
+        # Find the bin if no detection is found
+        # Align with the bin and move forward (through strafe should be fine)
+        # If we have lost sight of the bin, then end
+
+        # So we do not get a NoneType error
+        if detections is None:
+            detections = []
+        delete_id = []
+        for i in len(detections):
+            if detections(i).label == "Fire" or detections(i).label == "fire":
+                detections = []
+                break
+
+        if len(detections) == 0 and self.phase is None: #We are going to search
+            lateral, forward = search(frame, target, detections)
+            motion_commands_and_state = {
+                "lateral": lateral,
+                "forward": forward, 
+                "yaw": yaw, 
+                "vertical" : vertical, 
+                "drop": self.drop,
+                "wait": self.wait
+            }
+            return motion_commands_and_state, frame 
+        self.wait = False
+        if len(detections) == 0: 
+            self.state = 'missed'
+
+        if len(detections) >= 1:
+            self.phase = 'centering' #now we get to centering baby :)
+            if len(detections) == 1:
+                for detection in detections:
+                    print(f"[DEBUG] Detection confidence: {detection.confidence}")
+                    if detection.confidence > 0.65:
+                        target_x = (detection.xmin + detection.xmax) / 2
+                        target_y = (detection.ymin + detection.ymax) / 2
+                    # Might need to do something in an elif or else here
+                    else:
+                        target_x = None
+                
+            elif len(detections) > 1:
+                # Target the detection with the highest confidence. The detection targeted
+                # doesn't matter since this is a localization script, not a mission script
+
+                # Increased required confidence to 0.65 to account for multiple false positives without
+                # true positive
+                detection_confidence = 0.65
+                for detection in detections:
+                    if detection.confidence > detection_confidence:
+                        target_x = (detection.xmin + detection.xmax) / 2
+                        target_y = (detection.ymin + detection.ymax) / 2
+                        detection_confidence = detection.confidence
+        #if the target_x and target_y are within 50 pixles of the midpoints we are then close to being centered and can drop
+        if abs(target_x - self.x_midpoint) <= self.tolerance and abs(target_y - self.y_midpoint) <= self.tolerance: 
+            
+            self.drop = True
+            self.state = None
+            #since we are dropping we hold the sub in place (lines redundant)
+            forward = 0
+            lateral = 0
+            yaw = 0
+            vertical = 0
+
+        if abs(target_y - self.y_midpoint) > self.tolerance:
+            self.stage = 'lateral'
+        elif abs(target_x - self.x_midpoint) > self.tolerance:
+            self.stage = 'forward'
+        else:
+            self.stage = 'lateral'
+
+        #If no detection is made we enter missed mode, if we have a detection then we enter centering mode
+        if target_x is None or target_y is None:
+            self.state = "missed"
+        else:
+            self.prev_detected = True
+            self.state = "centering"
+
+
+        if self.state == "missed": 
+            self.missed_frames += 1
+            if self.missed_frames >= 100: #100 missed frames
+                self.missed_frames = 0
+                self.phase = None #go back to searching
+
+        if self.state == "centering":
+            if self.stage == 'lateral':
+                lateral = self.center_x(target_x)
+            if self.stage == 'forward':
+                forward = self.center_y(target_y)
+            
+
+        # Continuously return motion commands, the state of the mission, and the visualized frame.
+        motion_commands_and_state = {
+            "lateral": lateral,
+            "forward": forward, 
+            "yaw": yaw, 
+            "vertical" : vertical, 
+            "drop": self.drop, 
+            "wait": self.wait
+            }
+        return motion_commands_and_state, frame #end of run()
+        
+            
+            
+   
+            
---
+++ auv/mission/bins_final_mission.py
@@ -0,0 +1,164 @@
+"""
+bins_drop_mission: for now this code centers on one bin then drops both markers, further implementation to navigate is to be done
+once the base capability of dropping the marker into one bin is sucessful
+"""
+
+import json
+
+import rospy
+import time
+from std_msgs.msg import String
+
+from auv.device import cv_handler # For running mission-specific CV scripts
+from auv.motion import robot_control # For running the motors on the sub
+from auv.utils import arm, disarm
+
+class BinsDropMission:
+    cv_files = ["bins_final_cv"] # CV file to run
+
+    def __init__(self, target=None, **config):
+        """
+        Initialize the mission class; here should be all of the things needed in the run function. 
+
+        Args:
+            config: Mission-specific parameters to run the mission.
+        """
+        self.config = config
+        self.data = {}  # Dictionary to store the data from the CV handler
+        self.next_data = {}  # Dictionary to store the newest data from the CV handler; this data will be merged with self.data.
+        self.received = False
+        self.robot_control = robot_control.RobotControl()
+        self.heading = self.robot_control.get_heading
+
+        self.robot_control.set_flight_mode("STABLIZE")
+        self.robot_control.set_control_mode('depth_hold')
+
+        self.robot_control.go_to_depth(0.4) #change later for comp (0.2 is for house pool)
+        time.sleep(0.1)
+        
+        #self.robot_control.go_to_heading(self.heading)
+        self.robot_control.activate_heading_control(True)
+
+        self.cv_handler = cv_handler.CVHandler(**self.config)
+        # Initialize the CV handlers; dummys are used to input a video file instead of the camera stream as data for the CV script to run on
+        for file_name in self.cv_files:
+            self.cv_handler.start_cv(file_name, self.callback)
+
+        self.cv_handler.set_target("bins_final_cv", target)
+
+        
+
+    def callback(self, msg):
+        """
+        Calls back the cv_handler output -- you can have multiple callbacks for multiple CV handlers. Converts the output into JSON format.
+
+        Args:
+            msg: cv_handler output -- this will be a dictionary of motion commands and potentially the visualized frame as well as servo commands (like the torpedo launcher)
+        """
+        file_name = msg._connection_header["topic"].split("/")[-1] # Get the file name from the topic name
+        data = json.loads(msg.data) # Convert the data to JSON
+        self.next_data[file_name] = data 
+        self.received = True
+
+        # print(f"[DEBUG] Received data from {file_name}")
+
+
+    def move_servo(self):
+        try:
+            self.robot_control.move_servo("/auv/device/dropper")
+            time.sleep(0.5)
+            self.robot_control.move_servo("/auv/device/dropper")
+        except:
+            print('no servo detected')
+
+    def move_offset(self): #move the offset for marker dropper write once centering is good
+        pass
+
+
+    def run(self):
+        """
+        Here should be all the code required to run the mission.
+        This could be a loop, a finite state machine, etc.
+        """
+
+        while not rospy.is_shutdown():
+            time.sleep(0.05)
+            if not self.received:
+                continue
+
+            # Merge self.next_data, which contains the updated CV handler output, with self.data, which contains the previous CV handler output.
+            # self.next_data will be wiped so that it can be updated with the new CV handler output.
+            for key in self.next_data.keys():
+                if key in self.data.keys():
+                    self.data[key].update(self.next_data[key]) # Merge the data
+                else:
+                    self.data[key] = self.next_data[key] # Update the keys if necessary
+            self.received = False
+            self.next_data = {}
+
+            # Do something with the data.
+            lateral = self.data["bins_final_cv.py"].get("lateral", None)
+            forward = self.data["bins_final_cv.py"].get("forward", None)
+            yaw = self.data["bins_final_cv.py"].get("yaw", None)
+            vertical = self.data["bins_final_cv.py"].get("vertical", None)
+            drop = self.data["bins_final_cv.py"].get("drop", None)
+            wait = self.data["bins_final_cv.py"].get("wait", None)
+
+            if drop == True:
+                print("aligning dropper")
+                self.move_offset()
+                print("Bins Over, Dropping Now!")
+                self.robot_control.movement()
+                self.move_servo()
+            else:
+                if wait == True:
+                    time.sleep(1)
+                #if heading is fixed change this to robot_control.movement() with inputs of yaw lateral and forward
+                #self.robot_control.movement(yaw = yaw, forward = forward, lateral = lateral) replaced for DVL movement due to heading issues
+                self.robot_control.movement(forward=forward, lateral=lateral)
+                print('forward: ', forward, 'lateral: ', lateral)
+
+
+    def cleanup(self):
+        """
+        Here should be all the code required after the run function.
+        This could be cleanup, saving data, closing files, etc.
+        """
+        for file_name in self.cv_files:
+            self.cv_handler.stop_cv(file_name)
+
+        # Idle the robot and decrease depth for safetey
+        self.robot_control.movement(lateral = 0, forward = 0, yaw = 0)
+        self.robot_control.set_flight_mode("STABLIZE")
+        self.robot_control.go_to_depth(0.4)
+        print("[INFO] Bins drop mission terminate")
+
+
+if __name__ == "__main__":
+    # This is the code that will be executed if you run this file directly
+    # It is here for testing purposes
+    # you can run this file independently using: "python -m auv.mission.template_mission"
+    # You can also import it in a mission file outside of the package
+    import time
+    from auv.utils import deviceHelper
+    from auv.motion import robot_control
+    rospy.init_node("bins_final_cv", anonymous=True)
+
+    config = deviceHelper.variables
+    config.update(
+        {
+            # # this dummy video file will be used instead of the camera if uncommented
+            # "cv_dummy": ["/somepath/thisisavideo.mp4"],
+        }
+    )
+
+    # Create a mission object with arguments
+    mission = BinsDropMission(**config)
+
+    # Run the mission
+
+    arm.arm()
+    mission.run() #uncomment to do mission fr, just commented for servo test
+    mission.cleanup()
+
+    disarm.disarm()
